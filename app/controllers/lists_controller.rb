a9fab2fd97a86a2840990386297a947457108814
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-06-06 05:14:02 +0000

    9章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,23 @@ class ListsController < ApplicationController
 
  def create
   # 1.&2.データを受け取り新規登録するためのインスタンス作成
-  list = List.new(list_params)
+      # list = List.new(list_params)
   # 3.データをデータベースに保存するためのsaveメソッド実行
-  list.save
+      # list.save
   # 4.トップ画面へリダイレクト
   # redirect_to '/top'を削除して以下のコードに変更（ショウアクションにリダイレクトさせるため）
   #詳細画面へリダイレクト
-  redirect_to list_path(list.id)
+        # redirect_to list_path(list.id)
+
+  @list = List.new(list_params)
+  if @list.save
+   redirect_to list_path(@list.id)
+  else
+    # @lists = List.all
+    # render :index
+            # ↑ new から index に変更
+    render :new
+  end
 end
 
   def index
