3fbb9a1f59270d8b5d6addca7c3827be68494eb9
EC2 Default User <102804114+Natumesiki@users.noreply.github.com>
2022-06-06 13:37:27 +0000

    sample_app [finish]

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,5 +4,11 @@ class List < ApplicationRecord
   validates :title,presence: true
   validates :body, presence: true
   validates :image, presence: true
+　# ↑コメントアウトする
+# rails cで画像を扱う場合は複雑になるため、
+# Listモデルに設定してあるimageのバリデーションをコメントアウトし
+# タイトルと本文のみで投稿できるように変更します。
 
+# rails c 　確認　10章終わったら
+# コメントアウトを外す
 end
